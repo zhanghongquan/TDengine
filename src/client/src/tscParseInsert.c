f7e895c034ef1f69783ea37111428c2b61d801c7
yihaoDeng <yhdeng@taosdata.com>
2021-04-29 13:35:33 +0800

    enhance window client write performance

--- src/client/src/tscParseInsert.c
+++ src/client/src/tscParseInsert.c
@@ -703,17 +703,11 @@ static int32_t doParseInsertStatement(SSqlCmd* pCmd, char **str, STableDataBlock
   if (TSDB_CODE_SUCCESS != code) {
     return TSDB_CODE_TSC_OUT_OF_MEMORY;
   }
-
-  code = TSDB_CODE_TSC_INVALID_SQL;
-  char  *tmpTokenBuf = calloc(1, 16*1024);  // used for deleting Escape character: \\, \', \"
-  if (NULL == tmpTokenBuf) {
-    return TSDB_CODE_TSC_OUT_OF_MEMORY;
-  }
+  //TODO(yihaoDeng): refactor later
+  char tmpTokenBuf[16*1024] = {0};  // used for deleting Escape character: \\, \', \"
 
   int32_t numOfRows = 0;
   code = tsParseValues(str, dataBuf, maxNumOfRows, pCmd, &numOfRows, tmpTokenBuf);
-
-  free(tmpTokenBuf);
   if (code != TSDB_CODE_SUCCESS) {
     return code;
   }
